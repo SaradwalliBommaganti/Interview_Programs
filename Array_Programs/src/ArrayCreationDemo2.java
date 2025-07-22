f55948c17cbf63b48d1e913c3917883141e8ec5d
SaradwalliBommaganti <bommagantidharani@gmail.com>
2025-07-22 20:11:31 +0530

    created new demo

---
+++ Array_Programs/src/ArrayCreationDemo2.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+
+
+public class ArrayCreationDemo2 {
+
+
+      public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=2;
+       
+        arr[4]=2;
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+   
+
+}
