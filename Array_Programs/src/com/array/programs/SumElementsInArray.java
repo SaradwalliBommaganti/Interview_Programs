682aed0197f72f4aa3997ca8a063483555e6dbfa
SaradwalliBommaganti <bommagantidharani@gmail.com>
2025-07-23 22:54:44 +0530

    just improve my efficieny

--- Array_Programs/src/com/array/programs/SumElementsInArray.java
+++ Array_Programs/src/com/array/programs/SumElementsInArray.java
@@ -1,5 +1,13 @@
 package com.array.programs;
 
 public class SumElementsInArray {
-
+    public static void main(String args[]){
+        int arr[]={1,2,3};
+        int sum=0;
+        for(int x:arr){
+            sum=sum+x;
+           
+        }
+         System.out.println("sum: "+sum);
+    }
 }
