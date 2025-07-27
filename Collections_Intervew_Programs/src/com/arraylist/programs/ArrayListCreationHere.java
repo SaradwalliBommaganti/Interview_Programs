6152b86a0dd5ba341c6327efa0948d85e1a9df1a
SaradwalliBommaganti <bommagantidharani@gmail.com>
2025-07-27 13:06:23 +0530

    created done

---
+++ Collections_Intervew_Programs/src/com/arraylist/programs/ArrayListCreationHere.java
@@ -0,0 +1,16 @@
+package com.arraylist.programs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ArrayListCreationHere {
+	
+	public static void main(String[] args) {
+		List<String> names =  new ArrayList<>();
+		names.add("apple");
+		System.out.println(names.get(0));
+			
+	}
+
+}
